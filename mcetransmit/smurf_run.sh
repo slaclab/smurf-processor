fe64dadb8b359e81445780ee16054d3496779b38
Shawn W. Henderson <shawn@slac.stanford.edu>
2019-02-14 11:34:59 -0800

    Combined all of the smurf_*.sh scripts that start the pyrogue server into one script.  Can start the server without a gui now by providing the command-line --nogui option.  Tried to make all other inputs that change from setup to setup command-line options.  Default command-line options are for the current Stanford B09 campus setup.

--- mcetransmit/smurf_run.sh
+++ mcetransmit/smurf_run.sh
@@ -1,6 +1,60 @@
+#!/bin/bash
+
+#defaults
+SHELFMANAGER=10.0.1.4
+CRATEID=3
+SMURFSLOT=5
+NOGUI=
+
+PARAMS=""
+
+while (( "$#" )); do
+  case "$1" in
+    -m|--shm)
+      SHELFMANAGER=$2
+      shift 2
+      ;;
+    -c|--crateid)
+      CRATEID=$2
+      shift 2
+      ;;
+    -s|--smurfslot)
+      SMURFSLOT=$2
+      shift 2
+      ;;
+    --nogui)
+      NOGUI=-s
+      shift 1
+      ;;
+    --) # end argument parsing
+      shift
+      break
+      ;;
+    -*|--*=) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *) # preserve positional arguments
+      PARAMS="$PARAMS $1"
+      shift
+      ;;
+  esac
+done
+
+# set positional arguments in their proper place
+eval set -- "$PARAMS"
+
+# some derived inputs
+SMURFIP=10.0.${CRATEID}.$((100+${SMURFSLOT}))
+
+if screen -list | grep -q "pyrogue"
+then
+    screen -X -S pyrogue kill
+fi
 
 f="./log/$(date +"%FT%H%M%S")_smurf_run.log"
-amcc_dump_bsi --all 10.0.1.30/2 |& tee $f
-#scripts/control-server/start_server.sh -a 10.0.2.102 -c eth-rssi-interleaved -l 0 -t /usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/images/current.pyrogue.tar.gz -d /usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/config/defaults.yml -e test_epics -f Int16 -b 524288
-scripts/control-server/start_server.sh -a 10.0.2.102 -c pcie-rssi-interleaved -l 0 -t /usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/images/current.pyrogue.tar.gz -d /usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/config/defaults.yml -e test_epics -f Int16 -b 524288
+amcc_dump_bsi --all ${SHELFMANAGER}/${SMURFSLOT} |& tee $f
+
+screen -h 81920 -d -m -S pyrogue /home/cryo/smurf2mce/current/mcetransmit/scripts/control-server/start_server.sh -a ${SMURFIP} -c pcie-rssi-interleaved -l 0 -t /usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/images/current.pyrogue.tar.gz -d /usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/config/defaults.yml -e test_epics -f Int16 -b 524288 ${NOGUI} 
+
 
