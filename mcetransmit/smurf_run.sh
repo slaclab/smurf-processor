13e6c140acd86bb45ee677f6f9165c132a8ebbfc
Shawn W. Henderson <shawn@slac.stanford.edu>
2019-03-12 14:23:33 -0700

    Adding more options; can now hand epics_root, pyrogue tarball, and yml file on command line.

--- mcetransmit/smurf_run.sh
+++ mcetransmit/smurf_run.sh
@@ -3,12 +3,17 @@
 #defaults
 SHELFMANAGER=10.0.1.4
 CRATEID=3
-SMURFSLOT=5
+SMURFSLOT=2
 NOGUI=
 # PCIe interface is default
 INTERFACE=pcie-rssi-interleaved
 INTERM="screen -h 81920 -d -m -S pyrogue "
+EPICS_ROOT=test_epics
 PARAMS=""
+
+#ed07dde0
+DEFAULTS_YML=/usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/config/defaults.yml
+PYROGUE=/usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/images/current.pyrogue.tar.gz
 
 while (( "$#" )); do
   case "$1" in
@@ -26,6 +31,18 @@ while (( "$#" )); do
       ;;
     -s|--smurfslot)
       SMURFSLOT=$2
+      shift 2
+      ;;
+    -r|--epics-root)
+      EPICS_ROOT=$2
+      shift 2
+      ;;
+    -y|--defaults)
+      DEFAULTS_YML=$2
+      shift 2
+      ;;
+    -t|--pyrogue)
+      PYROGUE=$2
       shift 2
       ;;
     --nogui)
@@ -66,6 +83,11 @@ fi
 f="./log/$(date +"%FT%H%M%S")_smurf_run.log"
 amcc_dump_bsi --all ${SHELFMANAGER}/${SMURFSLOT} |& tee $f
 
-${INTERM} /home/cryo/smurf2mce/current/mcetransmit/scripts/control-server/start_server.sh -a ${SMURFIP} -c ${INTERFACE} -l ${RSSI_LINK} -t /usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/images/current.pyrogue.tar.gz -d /usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/config/defaults.yml -e test_epics -f Int16 -b 524288 ${NOGUI} 
+## ed07dde0
+echo ${INTERM} /home/cryo/smurf2mce/current/mcetransmit/scripts/control-server/start_server.sh -a ${SMURFIP} -c ${INTERFACE} -l ${RSSI_LINK} -t ${PYROGUE} -d ${DEFAULTS_YML} -e ${EPICS_ROOT} -f Int16 -b 524288 ${NOGUI} 
+## d4495370
+#${INTERM} /home/cryo/smurf2mce/current/mcetransmit/scripts/control-server/start_server.sh -a ${SMURFIP} -c ${INTERFACE} -l ${RSSI_LINK} -t /home/cryo/lbhb_fw_test2/MicrowaveMuxBpEthGen2-0x00000001-20190215083416-mdewart-d4495370.pyrogue.tar.gz -d /home/cryo/lbhb_fw_test2/defaults_lbhb_fw_test2_d4495370.yml -e ${EPICS_ROOT} -f Int16 -b 524288 ${NOGUI} 
+## 6752dc80
+#${INTERM} /home/cryo/smurf2mce/current/mcetransmit/scripts/control-server/start_server.sh -a ${SMURFIP} -c ${INTERFACE} -l ${RSSI_LINK} -t /home/cryo/shawn/fw_tests/lbhb_fw_test3/MicrowaveMuxBpEthGen2-0x00000001-20190308094902-mdewart-6752dc80.pyrogue.tar.gz -d /home/cryo/shawn/fw_tests/lbhb_fw_test3/defaults_lbhb_fw_test3_6752dc80_mmcswap.yml -e ${EPICS_ROOT} -f Int16 -b 524288 ${NOGUI} 
 
-
+## 3/12/19 attempting to run with 2x carriers.  Basically ignoring inputs.
