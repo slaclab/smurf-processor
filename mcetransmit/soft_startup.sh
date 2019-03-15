3288c11094962cec46871493c2f4fd8fe9926287
Shawn W. Henderson <shawn@slac.stanford.edu>
2019-03-15 15:56:59 -0700

    Moved startup example scripts to smurftestapps

--- mcetransmit/soft_startup.sh
+++
@@ -1,3 +0,0 @@
-./smurf_run.sh -m 10.0.1.4 -c 0 -s 2 -t /usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/images/current.pyrogue.tar.gz -y /usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/config/defaults.yml -r test_epics2 -i
-
-#./smurf_run.sh -m 10.0.1.4 -c 3 -s 5 -t /usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/images/current.pyrogue.tar.gz -y /usr/local/controls/Applications/smurf/cmb_Det/cryo-det/ultrascale+/firmware/targets/MicrowaveMuxBpEthGen2/config/defaults_hb.yml -r test_epics5
