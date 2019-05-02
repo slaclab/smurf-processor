b9c7e91037be692e05645b36330e95a990f11006
Jesus Vasquez <jesusvasquez333@gmail.com>
2019-05-01 17:30:54 -0700

    Update DataBuffer class - support more than one reader. The number of readers is set and construction - It is a circular buffer, overwriting data if it is not processed

--- src/data_buffer.cpp
+++ src/data_buffer.cpp
@@ -1,23 +1,26 @@
 
 #include "data_buffer.h"
 
-DataBuffer::DataBuffer(std::size_t s)
+DataBuffer::DataBuffer(std::size_t bufSize, std::size_t numReaders)
 :
-size     ( s     ),
-full     ( false ),
-empty    ( true  ),
-writeCnt ( 0     ),
-readCnt  ( 0     ),
-WOFCnt   ( 0     ),
-ROFCnt   ( 0     )
+size          ( bufSize           ),
+numberReaders ( numReaders        ),
+full          ( numReaders, false ),
+empty         ( numReaders, true  ),
+writeCnt      ( 0                 ),
+readCnt       ( numReaders, 0     ),
+OWCnt         ( numReaders, 0     ),
+ROFCnt        ( numReaders, 0     )
 {
     for (std::size_t i(0); i < size; ++i)
         data.push_back(ISmurfPacket::create());
 
     writePtr = data.begin();
-    readPtr = data.begin();
 
-    printf("DataBuffer created of size %zu", size);
+    for (std::size_t i(0); i < numberReaders; ++i)
+        readPtr.push_back(data.begin());
+
+    printf("DataBuffer created of size %zu, and number of readers %zu", size, numberReaders);
     printf("DataBuffeV2.size =  %zu\n", data.size());
 };
 
@@ -28,32 +31,33 @@ DataBuffer::~DataBuffer()
 
 SmurfPacket DataBuffer::getWritePtr()
 {
-    // Verify if the buffer is full
-    if (full)
+
+    // If a reader's buffer is full, update its overwrite counter and move it
+    // read pointer forward
+    for(std::size_t i(0); i < numberReaders; ++i)
     {
-        // Increase the write overflow counter and throw exception
-        ++WOFCnt;
-        throw std::runtime_error("Trying to write when the buffer is full");
+        if (full.at(i))
+        {
+            ++OWCnt.at(i);
+            ++readPtr.at(i);
+        }
     }
-    else
-    {
-        return *writePtr;
-    }
+
+    return *writePtr;
 };
 
-// Get a pointer to the next available data packet, ready to be processed.
-SmurfPacket_RO DataBuffer::getReadPtr()
+SmurfPacket_RO DataBuffer::getReadPtr(std::size_t i)
 {
     // Verify is the buffer is empty
-    if (empty)
+    if (empty.at(i))
     {
         // Increase the read overflow counter and throw exception
-        ++ROFCnt;
+        ++ROFCnt.at(i);
         throw std::runtime_error("Trying to read when the buffer is empty");
     }
     else
     {
-        return *readPtr;
+        return *readPtr.at(i);
     }
 };
 
@@ -68,12 +72,15 @@ void DataBuffer::doneWriting()
     if (writePtr == data.end())
         writePtr = data.begin();
 
-    // Verify if the buffer is full.
-    if (writePtr == readPtr)
-        full = true;
+    // Verify if the buffer is full for each reader.
+    for (std::size_t i(0); i < numberReaders; ++i)
+    {
+        if (writePtr == readPtr.at(i))
+            full.at(i) = true;
+    }
 
     // The buffer is not empty after writing new data into it.
-    empty = false;
+    std::fill(empty.begin(), empty.end(), false);
 
     // Update write counter
     ++writeCnt;
@@ -83,56 +90,62 @@ void DataBuffer::doneWriting()
     dataReady.notify_all();
 };
 
-void DataBuffer::doneReading()
+void DataBuffer::doneReading(std::size_t i)
 {
     // Move the iterator forward.
-    ++readPtr;
+    ++readPtr.at(i);
 
     // Verify is we arrived to the end of the buffer.
     // If so, move the iterator back to the start.
-    if (readPtr == data.end())
-        readPtr = data.begin();
+    if (readPtr.at(i) == data.end())
+        readPtr.at(i) = data.begin();
 
-    // Verify if the buffer is empty.
-    if (readPtr == writePtr)
-        empty = true;
+    // Verify if the buffer is empty for each reader.
+    if (readPtr.at(i) == writePtr)
+        empty.at(i) = true;
 
-    // The buffer is not full after reading data from it.
-    full = false;
+    // The buffer is not full after reading data from it for that reader.
+    full.at(i) = false;
 
     // Update read counter
-    ++readCnt;
+    ++readCnt.at(i);
 };
 
-const bool DataBuffer::isEmpty() const
+const bool DataBuffer::isEmpty(std::size_t i) const
 {
-    return empty;
+    return empty.at(i);
 };
 
 const bool DataBuffer::isFull() const
 {
-    return full;
+    return ( std::find(full.begin(), full.end(), true) != full.end() );
 };
 
-const int DataBuffer::getROFCnt() const
+const std::size_t DataBuffer::getROFCnt(std::size_t i) const
 {
-    return ROFCnt;
+    return ROFCnt.at(i);
 };
 
-const int DataBuffer::getWOFCnt() const
+const std::size_t DataBuffer::getOWCnt(std::size_t i) const
 {
-    return WOFCnt;
+    return OWCnt.at(i);
 };
 
-void DataBuffer::clearOFCnts()
+void DataBuffer::clearCnts()
 {
-    ROFCnt = 0; WOFCnt =0;
+    std::fill(ROFCnt.begin(), ROFCnt.end(), 0);
+    std::fill(OWCnt.begin(), OWCnt.end(), 0);
 };
 
 const std::size_t DataBuffer::getSize() const
 {
     return size;
 };
+
+const std::size_t DataBuffer::getNumReaders() const
+{
+    return numberReaders;
+}
 
 std::mutex* DataBuffer::getMutex()
 {
@@ -151,10 +164,23 @@ void DataBuffer::printStatistic() const
     std::cout << "------------------------------"                                << std::endl;
     std::cout << "Buffer size                     : " << size                    << std::endl;
     std::cout << "Total write operations          : " << writeCnt                << std::endl;
-    std::cout << "Total read operations           : " << readCnt                 << std::endl;
-    std::cout << "Total write attempts while full : " << WOFCnt                  << std::endl;
-    std::cout << "Total read attempts while empty : " << ROFCnt                  << std::endl;
-    std::cout << "Buffer 'empty' flag             : " << std::boolalpha << empty << std::endl;
-    std::cout << "Buffer 'full' flag              : " << std::boolalpha << full  << std::endl;
-    std::cout << "------------------------------"                                << std::endl;
+
+    std::cout << "Total read operations           : ";
+    std::copy(readCnt.begin(), readCnt.end(), std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << std::endl;
+
+    std::cout << "Total Overwrites                : ";
+    std::copy(OWCnt.begin(), OWCnt.end(), std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << std::endl;
+
+    std::cout << "Total read attempts while empty : ";
+    std::copy(ROFCnt.begin(), ROFCnt.end(), std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << std::endl;
+
+    std::cout << "Buffer 'empty' flag             : " << std::boolalpha;
+    std::copy(empty.begin(), empty.end(), std::ostream_iterator<bool>(std::cout, ", "));
+    std::cout << std::endl;
+
+    std::cout << "Buffer 'full' flag              : " << std::boolalpha << isFull()  << std::endl;
+    std::cout << "------------------------------"                                    << std::endl;
 };
