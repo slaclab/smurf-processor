b931934082d330193cf4935e7e5a7c6ba87aaa40
Jesus Vasquez <jvasquez@slac.stanford.edu>
2019-09-20 14:19:52 -0700

    Update start_server.sh script

--- scripts/control-server/start_server.sh
+++ scripts/control-server/start_server.sh
@@ -1,110 +1,265 @@
 #!/usr/bin/env bash
-#-----------------------------------------------------------------------------
-# Title      : PyRogue Server Startup Script
-#-----------------------------------------------------------------------------
-# File       : start_server.sh
-# Created    : 2017-06-20
-#-----------------------------------------------------------------------------
-# Description:
-# Bash script wrapper to start a PyRogue Server
-#-----------------------------------------------------------------------------
-# This file is part of the pyrogue-control-server software platform. It is subject to
-# the license terms in the LICENSE.txt file found in the top-level directory
-# of this distribution and at:
-#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
-# No part of the rogue software platform, including this file, may be
-# copied, modified, propagated, or distributed except according to the terms
-# contained in the LICENSE.txt file.
-#-----------------------------------------------------------------------------
 
-# workaround to start in detached screen
-source /home/cryo/.bashrc
+###############
+# Definitions #
+###############
+# Shell PID
+top_pid=$$
 
-# File name definitions
-SCRIPT_NAME=$0
-TOP=$(dirname -- "$(readlink -f $0)")
-PYTHON_SCRIPT_NAME=$TOP/python/pyrogue_server.py
-SETUP_SCRIPTS=$TOP/setup*.sh
+# This script name
+script_name=$(basename $0)
 
-# MCE library location
-MCE_LIB_PATH=$TOP/../../lib/
+# Firmware file location
+fw_top_dir="/tmp/fw"
 
-# Setup initial enviroment
-export PYTHONPATH=$PYTHONPATH:$MCE_LIB_PATH
+########################
+# Function definitions #
+########################
+
+# Trap TERM signals and exit
+trap "echo 'An ERROR was found. Aborting...'; exit 1" TERM
 
 # Usage message
-usage() {
+usage()
+{
+    echo "Start the SMuRF server on a specific board."
     echo ""
-    echo "Start a PyRogue server to communicate with an FPGA."
-    echo "This startup bash script set the enviroinment and calls the python script $PYTHON_SCRIPT_NAME"
+    echo "usage: ${script_name} [-S|--shelfmanager <shelfmanager_name> -N|--slot <slot_number>]"
+    echo "                      [-a|--addr <FPGA_IP>] [-D|--no-check-fw] [-g|--gui] <pyrogue_server-args>"
+    echo "    -S|--shelfmanager <shelfmanager_name> : ATCA shelfmanager node name or IP address. Must be used with -N."
+    echo "    -N|--slot         <slot_number>       : ATCA crate slot number. Must be used with -S."
+    echo "    -a|--addr         <FPGA_IP>           : FPGA IP address. If defined, -S and -N are ignored."
+    echo "    -D|--no-check-fw                      : Disabled FPGA version checking."
+    echo "    -g|--gui                              : Start the server with a GUI."
+    echo "    -h|--help                             : Show this message."
+    echo "    <pyrogue_server_args> are passed to the SMuRF pyrogue server. "
     echo ""
-    echo "Usage: $SCRIPT_NAME -t|--tar <pyrogue.tar.gz> [-h|--help] {extra arguments for $PYTHON_SCRIPT_NAME}"
-    echo "    -t|--tar <pyrogue.tar.gz> : tarball file with pyrogue definitions."
-    echo "    -h|--help                 : Show this message"
+    echo "If -a if not defined, then -S and -N must both be defined, and the FPGA IP address will be automatically calculated from the crate ID and slot number."
+    echo "If -a if defined, -S and -N are ignored."
+    echo
+    echo "The script will bu default check if the firmware githash read from the FPGA via IPMI is the same of the found in the MCS file name."
+    echo "This checking can be disabled with -D. The checking will also be disabled if -a is used instead of -S and -N."
+    echo
+    echo "By default, the SMuRF server is tarted without a GUI (server mode). Use -g to start the server with a GUI. -s|--server option is ignored as that is the default."
+    echo
+    echo "All other arguments are passed verbatim to the SMuRF server."
     echo ""
-    echo "All other arguments are passed directly to $PYTHON_SCRIPT_NAME which usage is:"
-    echo ""
-    $PYTHON_SCRIPT_NAME -h
-    exit
+    exit 1
 }
 
-# Check for arguments
-ARGS=""
+getGitHashFW()
+{
+    local gh_inv
+    local gh
+
+    # Long githash (inverted)
+    #gh_inv=$(ipmitool -I lan -H $SHELFMANAGER -t $IPMB -b 0 -A NONE raw 0x34 0x04 0xd0 0x14  2> /dev/null)
+    # Short githash (inverted)
+    gh_inv=$(ipmitool -I lan -H $shelfmanager -t $ipmb -b 0 -A NONE raw 0x34 0x04 0xe0 0x04  2> /dev/null)
+
+    if [ "$?" -ne 0 ]; then
+        kill -s TERM ${top_pid}
+    fi
+
+    # Invert the string
+    for c in ${gh_inv} ; do gh=${c}${gh} ; done
+
+    # Return the short hash (7 bytes)
+    echo ${gh} | cut -c 1-7
+}
+
+getGitHashMcs()
+{
+    local filename=$(basename $mcs_file_name)
+    local gh=$(echo $filename | sed  -r 's/.+-+(.+).mcs.*/\1/')
+
+    # Return the short hash (7 bytes)
+    echo ${gh} | cut -c 1-7
+}
+
+getCrateId()
+{
+    local crate_id_str
+
+    crate_id_str=$(ipmitool -I lan -H $shelfmanager -t $ipmb -b 0 -A NONE raw 0x34 0x04 0xFD 0x02 2> /dev/null)
+
+    if [ "$?" -ne 0 ]; then
+        kill -s TERM ${top_pid}
+    fi
+
+    local crate_id=`printf %04X  $((0x$(echo $crate_id_str | awk '{ print $2$1 }')))`
+
+    if [ -z ${crate_id} ]; then
+        kill -s TERM ${top_pid}
+    fi
+
+    echo ${crate_id}
+}
+
+getFpgaIp()
+{
+
+    # Calculate FPGA IP subnet from the crate ID
+    local subnet="10.$((0x${crate_id:0:2})).$((0x${crate_id:2:2}))"
+
+    # Calculate FPGA IP last octect from the slot number
+    local fpga_ip="${subnet}.$(expr 100 + $slot)"
+
+    echo ${fpga_ip}
+}
+
+#############
+# Main body #
+#############
+
+# Verify inputs arguments
 while [[ $# -gt 0 ]]
 do
-    key="$1"
-    case $key in
-        -t|--tar)
-            # Read the tarball file argument
-            TAR_FILE="$2"
-            shift
-            ;;
-        -h|--help)
-            # Capture the help argument
-            usage
-            ;;
-        *)
-            # All other arguemnts are passed to the pyton script
-            ARGS="$ARGS $1"
-            #shift
-            ;;
-    esac
+key="$1"
+
+case ${key} in
+    -S|--shelfmanager)
+    shelfmanager="$2"
     shift
+    ;;
+    -N|--slot)
+    slot="$2"
+    shift
+    ;;
+    -D|--no-check-fw)
+    no_check_fw=1
+    ;;
+    -a|--addr)
+    fpga_ip="$2"
+    shift
+    ;;
+    -g|--gui)
+    use_gui=1
+    ;;
+    -s|--server)
+    ;;
+    -h|--help)
+    usage
+    ;;
+    *)
+    args="${args} $key"
+    ;;
+esac
+shift
 done
 
-echo ""
+echo
 
-# The tarball is required
-if [ ! -f "$TAR_FILE" ]
-then
-    echo "Tar file not found!"
-    exit
+# Verify mandatory parameters
+
+# Check IP address or shelfmanager/slot number
+if [ -z ${fpga_ip+x} ]; then
+    # If the IP address is not defined, shelfmanager and slot numebr must be defined
+
+    if [ -z ${shelfmanager+x} ]; then
+        echo "Shelfmanager not defined!"
+        usage
+    fi
+
+    if [ -z ${slot+x} ]; then
+        echo "Slot number not defined!"
+        usage
+    fi
+
+    echo "IP address was not defined. It will be calculated automatically from the crate ID and slot number..."
+
+    ipmb=$(expr 0128 + 2 \* $slot)
+
+    echo "Reading Crate ID via IPMI..."
+    crate_id=$(getCrateId)
+    echo "Create ID: ${crate_id}"
+
+    echo "Calculating FPGA IP address..."
+    fpga_ip=$(getFpgaIp)
+    echo "FPGA IP: ${fpga_ip}"
+
+else
+    echo "IP address was defined. Ignoring shelfmanager and slot number. FW version checking disabled."
+    no_check_fw=1
 fi
 
-# Untar the pyrogue definitions
-TEMP_DIR=/tmp/$USER/pyrogue
-rm -rf $TEMP_DIR
-mkdir -p $TEMP_DIR
-echo "Untaring pyrogue tarball into $TEMP_DIR"
-tar -zxf  $TAR_FILE -C $TEMP_DIR
+# Add the IP address to the SMuRF arguments
+args="${args} -a ${fpga_ip}"
 
-# Get the pyrogue directory path
-PROJ=$(ls $TEMP_DIR)
-DIR=$TEMP_DIR/$PROJ
-echo "Project name = $PROJ"
-echo "PyRogue directory = $DIR"
+# Check if the server GUI was requested
+if [ -z ${use_gui+x} ]; then
+    args="${args} -s"
+else
+    echo "Server GUI enabled."
+fi
 
-# Setup the enviroment
-echo ""
-echo "Setting the enviroment..."
-for f in $SETUP_SCRIPTS; do
-	[ -e "$f" ] && echo "Sourcing $f..." && source $f
-done
-export PYTHONPATH=$PYTHONPATH:$DIR/python
+# If the slot number is defined, add the RSSI link number argument
+# which is needed if the PCIe card is used for communication
+if [ ${slot+x} ]; then
+    # Verify that the slot number is in the range [2,7]
+    if [ ${slot} -ge 2 -a ${slot} -le 7 ]; then
+        args="${args} -l $((slot-2))"
+    else
+        echo "Invalid slot number! Must be a number between 2 and 7."
+        exit 1
+    fi
+fi
 
-# Start the server
-echo ""
-echo "Starting the server..."
-CMD="$PYTHON_SCRIPT_NAME $ARGS"
-echo $CMD
-$CMD
+# Extract the pyrogue tarball and update PYTHONPATH
+echo "Looking for pyrogue tarball..."
+pyrogue_file=$(find ${fw_top_dir} -maxdepth 1 -name *pyrogue.tar.gz)
+if [ ! -f "$pyrogue_file" ]; then
+    pyrogue_file=$(find ${fw_top_dir} -maxdepth 1 -name *python.tar.gz)
+    if [ ! -f "$pyrogue_file" ]; then
+        echo "Pyrogue tarball file not found!"
+        exit 1
+    fi
+fi
+echo "Pyrogue file found: ${pyrogue_file}"
+
+echo "Extracting the pyrogue tarball into ${fw_top_dir}/pyrogue..."
+rm -rf ${fw_top_dir}/pyrogue
+mkdir ${fw_top_dir}/pyrogue
+tar -zxf ${pyrogue_file} -C ${fw_top_dir}/pyrogue
+proj=$(ls ${fw_top_dir}/pyrogue)
+export PYTHONPATH=${fw_top_dir}/pyrogue/${proj}/python:${PYTHONPATH}
+echo "Done. Pyrogue extracted to ${fw_top_dir}/pyrogue/${proj}."
+
+# Firmware version checking
+if [ -z ${no_check_fw+x} ]; then
+
+    mcs_file=$(find ${fw_top_dir} -maxdepth 1 -name *mcs*)
+    if [ ! -f "${mcs_file}" ]; then
+        echo "MCS file not found!"
+        exit 1
+    fi
+
+    mcs_file_name=$(basename ${mcs_file})
+    echo ${mcs_file_name}
+
+    echo "Reading FW Git Hash via IPMI..."
+    fw_gh=$(getGitHashFW)
+    echo "Firmware githash: '$fw_gh'"
+
+    echo "Reading MCS file Git Hash..."
+    mcs_gh=$(getGitHashMcs)
+    echo "MCS file githash: '$mcs_gh'"
+
+    if [ "${fw_gh}" == "${mcs_gh}" ]; then
+        echo "They match..."
+    else
+        echo "They don't match. Loading image..."
+        ProgramFPGA.bash -s $shelfmanager -n $slot -m $mcs_file
+    fi
+
+else
+    echo "Check firmware disabled."
+fi
+
+# MCE library location
+SMURF_PROCESSOR_LIB_PATH=/usr/local/src/smurf-processor/lib/
+export PYTHONPATH=$SMURF_PROCESSOR_LIB_PATH:${PYTHONPATH}
+
+echo "Starting server..."
+cd /data/smurf-processor_config/
+/usr/local/src/smurf-processor/scripts/control-server/python/pyrogue_server.py ${args}
