c75b8702b9c4a8cf2fdac78b004730f9cfaa9e6d
Jesus Vasquez <jesusvasquez333@gmail.com>
2019-09-30 15:53:40 -0700

    Add python wrappers

--- python/pysmurf/core/filter/_Filter.py
+++ python/pysmurf/core/filter/_Filter.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 #-----------------------------------------------------------------------------
-# Title      : PySMuRF Python Package Directory File
+# Title      : PySMuRF Data Filter
 #-----------------------------------------------------------------------------
 # File       : __init__.py
 # Created    : 2019-09-30
 #-----------------------------------------------------------------------------
 # Description:
-#    Mark this directory as python package directory.
+#    SMuRF Data Filter Python Package
 #-----------------------------------------------------------------------------
 # This file is part of the smurf software platform. It is subject to
 # the license terms in the LICENSE.txt file found in the top-level directory
@@ -16,3 +16,22 @@
 # copied, modified, propagated, or distributed except according to the terms
 # contained in the LICENSE.txt file.
 #-----------------------------------------------------------------------------
+
+import pyrogue
+import smurf
+
+class Filter(pyrogue.Device):
+    """
+    SMuRF Data Filter Python Wrapper.
+    """
+    def __init__(self, size, name, **kwargs):
+        pyrogue.Device.__init__(self, name=name, description='SMuRF Data Filter', **kwargs)
+        self._filter = smurf.core.filter.Filter(size)
+
+    # Method called by streamConnect, streamTap and streamConnectBiDir to access slave
+    def _getStreamSlave(self):
+        return self._filter
+
+    # Method called by streamConnect, streamTap and streamConnectBiDir to access master
+    def _getStreamMaster(self):
+        return self._filter
--- python/pysmurf/core/reorderer/_Reorderer.py
+++ python/pysmurf/core/reorderer/_Reorderer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 #-----------------------------------------------------------------------------
-# Title      : PySMuRF Python Package Directory File
+# Title      : PySMuRF Data Re-orderer
 #-----------------------------------------------------------------------------
 # File       : __init__.py
 # Created    : 2019-09-30
 #-----------------------------------------------------------------------------
 # Description:
-#    Mark this directory as python package directory.
+#    SMuRF Data Re-orderer Python Package
 #-----------------------------------------------------------------------------
 # This file is part of the smurf software platform. It is subject to
 # the license terms in the LICENSE.txt file found in the top-level directory
@@ -16,3 +16,22 @@
 # copied, modified, propagated, or distributed except according to the terms
 # contained in the LICENSE.txt file.
 #-----------------------------------------------------------------------------
+
+import pyrogue
+import smurf
+
+class Reorderer(pyrogue.Device):
+    """
+    SMuRF Data Re-orderer Python Wrapper.
+    """
+    def __init__(self, name, **kwargs):
+        pyrogue.Device.__init__(self, name=name, description='SMuRF Data Re-orderer', **kwargs)
+        self._filter = smurf.core.reorderer.Reorderer()
+
+    # Method called by streamConnect, streamTap and streamConnectBiDir to access slave
+    def _getStreamSlave(self):
+        return self._filter
+
+    # Method called by streamConnect, streamTap and streamConnectBiDir to access master
+    def _getStreamMaster(self):
+        return self._filter
