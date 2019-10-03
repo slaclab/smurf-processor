88470e5668cdd312d774ce0b6df3b7dfff315d8e
Jesus Vasquez <jesusvasquez333@gmail.com>
2019-10-03 12:09:38 -0700

    Add missing data types definitions

--- include/smurf/core/filters/GeneralAnalogFilter.h
+++ include/smurf/core/filters/GeneralAnalogFilter.h
@@ -73,6 +73,12 @@ namespace smurf
                 const std::size_t getNumCh() const;
 
             private:
+                // Data type used to read the data from the input frame
+                typedef int32_t input_data_t;
+
+                // Data type used to write data to the output frame
+                typedef int32_t output_data_t;
+
                 std::size_t         numCh;  // Number of channels being processed
                 std::size_t         order;  // Filter order
                 double              gain;   // Filter gain
